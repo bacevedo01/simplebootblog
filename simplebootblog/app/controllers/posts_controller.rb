192ffe704db36497bb93b7e21e09e31c540864f4
Benjamin Acevedo <benjaminacevedo@Benjamins-MacBook-Pro-2.local>
2017-08-31 16:24:14 -0700

    Add delete functionality.

--- simplebootblog/app/controllers/posts_controller.rb
+++ simplebootblog/app/controllers/posts_controller.rb
@@ -38,7 +38,10 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
 
+		redirect_to posts_path
 	end
 
 	private def post_params
