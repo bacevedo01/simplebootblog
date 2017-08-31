31e6f50cda98cbe2debcc12a95afec27f0933c54
Benjamin Acevedo <benjaminacevedo@Benjamins-MacBook-Pro-2.local>
2017-08-31 16:13:29 -0700

    Add edit functionality and show page.

--- simplebootblog/app/controllers/posts_controller.rb
+++ simplebootblog/app/controllers/posts_controller.rb
@@ -9,19 +9,40 @@ class PostsController < ApplicationController
 	end
 
 	def new
-
+		@post = Post.new
 	end
 
 	def create
 		#render plain: params[:post], inspect
 		@post = Post.new(post_params)
 
-		@post.save
-		redirect_to @post
+		if (@post.save)
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if (@post.update(post_params))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+
+	end
 
 	private def post_params
 		params.require(:post).permit(:title, :body)
 	end
+
 end
